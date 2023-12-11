8d03f7964c535eaaeb285728e735841fb1da0b86
AznRaven <74807576+AznRaven@users.noreply.github.com>
2023-12-10 23:19:36 -0600

    click for checkbox

--- app/(components)/Todo.js
+++ app/(components)/Todo.js
@@ -12,12 +12,12 @@ const Todo = ({ todo }) => {
   return (
     <>
       <li className="p-3 my-5 bg-slate-300 flex justify-between shadow-lg" key={todo.id}>
-        <div>
-          <input className="w-10 h-10 me-5" type="checkbox" />
-          <div>
+        <div className="flex grow">
+          <input id={todo.id} className="w-10 h-10 me-5" type="checkbox" />
+          <label htmlFor={todo.id} className="grow cursor-pointer">
             <h1 className="text-2xl font-bold">{todo.title}</h1>
             <p>{todo.item}</p>
-          </div>
+          </label>
         </div>
         <div className="flex flex-col justify-around">
           <button
